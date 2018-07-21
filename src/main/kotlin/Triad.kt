bfe4f725752f34641b61a879b86fac9e80cc9918
Nick Rose <nicholas.rose@cbc.ca>
2018-07-21 07:16:24 -0400

    Add failure cases for non major or minor triads

--- src/main/kotlin/Triad.kt
+++ src/main/kotlin/Triad.kt
@@ -1,40 +1,35 @@
-data class Triad(
-        val root: PitchClass,
-        val third: PitchClass,
-        val fifth: PitchClass) {
+data class Triad(val root: PitchClass,
+                 val third: PitchClass,
+                 val fifth: PitchClass) {
 
-    fun parallel() = Triad(
-            root,
-            if (isMinor()) third + 1 else third - 1,
-            fifth
-    )
+    fun parallel() = Triad(root = root,
+            third = if (isMinor()) third + 1
+                    else if (isMajor()) third - 1
+                    else onNonMinorOrMajorTriad(),
+            fifth = fifth)
 
     fun relative() = if (isMinor()) {
-        Triad(
-                root = third,
+        Triad(root = third,
                 third = fifth,
-                fifth = root.transpose(6, 10)
-        )
-    } else {
-        Triad(
-                root = fifth.transpose(1, 2),
+                fifth = root.transpose(6, 10))
+    } else if (isMajor()) {
+        Triad(root = fifth.transpose(1, 2),
                 third = root,
-                fifth = third
-        )
+                fifth = third)
+    } else {
+        onNonMinorOrMajorTriad()
     }
 
     fun leadingTone() = if (isMinor()) {
-        Triad(
-                root = fifth.transpose(1, 1),
+        Triad(root = fifth.transpose(1, 1),
                 third = root,
-                fifth = third
-        )
-    } else {
-        Triad(
-                root = third,
+                fifth = third)
+    } else if (isMajor()) {
+        Triad(root = third,
                 third = fifth,
-                fifth = root.transpose(6, 11)
-        )
+                fifth = root.transpose(6, 11))
+    } else {
+        onNonMinorOrMajorTriad()
     }
 
     fun nebenverwandt() = relative().leadingTone().parallel()
@@ -43,7 +38,10 @@ data class Triad(
 
     fun hexatonicPole() = leadingTone().parallel().leadingTone()
 
-    fun isMinor() = root.isMinorThird(third)
+    fun isMinor() = root.isMinorThird(third) && third.isMajorThird(fifth)
 
-    fun isMajor() = root.isMajorThird(third)
+    fun isMajor() = root.isMajorThird(third) && third.isMinorThird(fifth)
+
+    private fun onNonMinorOrMajorTriad(): Nothing =
+            error("Triad was neither minor nor major")
 }
